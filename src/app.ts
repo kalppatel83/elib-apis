1bc90d7dea0a88a05aef499401ffd20a8987d897
Kalp Patel <220133107026@gecg28.ac.in>
2024-08-25 00:18:53 +0530

    added global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
@@ -6,7 +7,10 @@ const app = express();
 // Http methods : GET,POST,PUT,PATCH,DELETE
 
 app.get("/", (req, res, next) => {
+  // throw new Error("Something went wrong");
+  // const error = createHttpError(400, "Something went wrong");
+  // throw error;
   res.json({ message: "Welcome to elib apis" });
 });
-
+app.use(globalErrorHandler);
 export default app;
