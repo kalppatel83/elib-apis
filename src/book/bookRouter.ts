ac932492258fdbddf747fb23fe6aa6a06597c3dd
Kalp Patel <220133107026@gecg28.ac.in>
2024-08-25 20:07:49 +0530

    setup Multer

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,7 +1,21 @@
 import express from "express";
 const bookRouter = express.Router();
 import createBook from "./bookController";
+import multer from "multer";
+import path from "node:path";
 
+// file store local(needed to give location) => s3(cloud) => then delete local file
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 },
+});
 // Routes
-bookRouter.post("/", createBook);
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 export default bookRouter;
