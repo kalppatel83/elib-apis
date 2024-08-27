0889c96d35d7ca76376a58fd18c1cb49fb9f82d2
Kalp Patel <220133107026@gecg28.ac.in>
2024-08-27 18:10:01 +0530

    Create Book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,23 +2,24 @@ import { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import fs from "node:fs";
+import bookModel from "./bookModel";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  // const {} = req.body;
+  const { title, genre } = req.body;
   // console.log("files", req.files);
   // stackOverflow to typecast so ts won't give error might be undefined [3:29:00]
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
 
   try {
-    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
-    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
-
-    const fileName = files.coverImage[0].filename;
-    const filePath = path.resolve(
-      __dirname,
-      "../../public/data/uploads",
-      fileName
-    );
-
     const uploadResult = await cloudinary.uploader.upload(filePath, {
       filename_override: fileName,
       folder: "book-covers",
@@ -44,10 +45,25 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
 
     console.log("uploadResult", uploadResult);
     console.log("bookFileUploadResult", bookFileUploadResult);
+
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "66cafcae13948eaf42adcf0f",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    // Delete temp.files
+    // todo: wrap in try catch...
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookFilePath);
+
+    res.status(201).json({ id: newBook._id });
   } catch (error) {
-    return next(createHttpError(500, "Error while uploading files"));
+    // console.log(error);
+    return next(createHttpError(500, "Error while uploading the files."));
   }
-  res.json({});
 };
 
 export default createBook;
